236c728a2d23c9d45685196dadb24dc98984696b
erand-palliyaguru <erandasampath2000@gmail.com>
2021-11-23 13:11:38 +0530

    up

--- set_average_sales.php
+++ set_average_sales.php
@@ -7,7 +7,7 @@ $result1 = $db->prepare("SELECT SUM(qty),item_code FROM INVOICE_DTL GROUP BY ite
 $result1->bindParam(':userid', $res);
 $result1->execute();
 for($i=0; $row1 = $result1->fetch(); $i++){
-echo $row1['item_code']."____".$row1['SUM(qty)'];
+echo $row1['item_code']."____".$row1['SUM(qty)']."<br>";
 }
 
 ?>
