033e14b8e08bc4693d6bcf6d4a36546a30e3217e
erand-palliyaguru <erandasampath2000@gmail.com>
2021-11-30 12:40:17 +0530

    cha 2

--- date_set_dtl.php
+++ date_set_dtl.php
@@ -3,7 +3,7 @@ session_start();
 include('../connect.php');
 
 
-$stmt = $db->query("SELECT date, sys_invo FROM sales  ");
+$stmt = $db->query("SELECT date, sys_invo FROM sales WHERE date BETWEEN '2019-01-01' AND '2019-12-31' ");
 while ($row1 = $stmt->fetch()){
 $sys_invo=$row1['sys_invo'];
 $date=$row1['date'];
