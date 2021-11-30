f800c420813e390eeed6a0fb74c613ea01c043eb
erand-palliyaguru <erandasampath2000@gmail.com>
2021-11-30 14:06:00 +0530

    bb

--- date_set_dtl.php
+++ date_set_dtl.php
@@ -3,7 +3,7 @@ session_start();
 include('../connect.php');
 
 
-$stmt = $db->query("SELECT TOP 100 , date, id, invo_no FROM sales WHERE ch = '0' ");
+$stmt = $db->query("SELECT  date, id, invo_no FROM sales WHERE ch = '0' LIMIT 100 ");
 while ($row1 = $stmt->fetch()){
 $sys_invo=$row1['invo_no'];
 $date=$row1['date'];
@@ -16,7 +16,12 @@ WHERE sys_invo=? ";
 $q = $db->prepare($sql);
 $q->execute(array($date,$sys_invo));
 
-
+$ch="1";
+$sql = "UPDATE sales 
+SET ch=?
+WHERE id=? "; 
+$q = $db->prepare($sql);
+$q->execute(array($ch,$id));
 
 }
 
