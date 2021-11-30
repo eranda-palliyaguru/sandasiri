dabff33ee830cba26f2e201499b668e8ed102dc6
erand-palliyaguru <erandasampath2000@gmail.com>
2021-11-30 13:49:03 +0530

    ch

--- date_set_dtl.php
+++ date_set_dtl.php
@@ -3,7 +3,7 @@ session_start();
 include('../connect.php');
 
 
-$stmt = $db->query("SELECT TOP 100 date, id, invo_no FROM sales WHERE ch ='' ");
+$stmt = $db->query("SELECT TOP 100 date, id, invo_no FROM sales WHERE ch ='0' ");
 while ($row1 = $stmt->fetch()){
 $sys_invo=$row1['invo_no'];
 $date=$row1['date'];
