f046bedf15c3f87b49da8415643393d948d31ad6
erand-palliyaguru <erandasampath2000@gmail.com>
2021-12-09 17:16:39 +0530

    up

--- sidebar.php
+++ sidebar.php
@@ -29,7 +29,7 @@
         </div>
         <div class="info">
           <a href="#" class="d-block">  <?php echo $_SESSION["name"];?> - <?php //  echo $_SESSION['SESS_LAST_NAME'];?></a>
-<form action="index.php" method="post" accept-charset="utf-8">
+          <form action="../auth/logout.php" method="post" accept-charset="utf-8">
           <input type="hidden" name="action" value="logOut" />
           <button type="submit" class="btn btn-block btn-outline-primary">Log out</button>
         </form>
