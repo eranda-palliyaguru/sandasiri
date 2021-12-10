99b0eb07aa8152ba1332d97e7cbe79fd59a8b7b8
erand-palliyaguru <erandasampath2000@gmail.com>
2021-12-10 18:09:48 +0530

    up

--- product.php
+++ product.php
@@ -121,7 +121,7 @@ function showResult(str) {
                     <td><?php echo $row2['qty'] ?></td>
                     <td><?php echo $row2['capacity'] ?></td>
                     <td><?php echo round($row2['capacity'] - $row2['qty']); ?></td>
-                    <td><?php $type=$row2['capacity_type']; if($type==1){echo"Medium";} if($type==2){ if($row2['capacity']==0){echo"Bad";}else{echo"Low";} } ?></td>
+                    <td><?php $type=$row2['capacity_type']; if($type==1){echo"<b style='color:#1A65F8;'>Medium</b>";} if($type==2){ if($row2['capacity']==0){echo"<b style='color:red;'>Bad</b>";}else{echo"<b style='color:yellow;'>Low</b>";} } ?></td>
                     <td><a href="product_profile.php?id=<?php  echo $row2['id']; ?>" class="btn btn-primary btn-sm">Profile</a></td>
                     
                     </tr>
