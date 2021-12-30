901480ec79bea5cb17f8d09c623faead5ec0d7d0
erand-palliyaguru <erandasampath2000@gmail.com>
2021-12-30 16:54:30 +0530

    up

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@ $upic1=$row1['u_pic'];
     <!-- Brand Logo -->
     <a href="dashboard" class="brand-link">
       <img src="../dist/img/AdminLTELogo.png" alt="CLOUD ARM" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">CLOUD arm</span>
+      <span class="brand-text font-weight-light">CLOUD ARM</span>
     </a>
 
     <!-- Sidebar -->
@@ -28,7 +28,7 @@ $upic1=$row1['u_pic'];
           <img src="user_pic/<?php  echo $upic1;?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"> <h4><?php echo $_SESSION["name"];?> - <?php   echo $_SESSION['POSITION'];?></h4> </a>
+          <a href="#" class="d-block"> <h5><?php echo $_SESSION["name"];?> - <?php   echo $_SESSION['POSITION'];?></h5> </a>
           <form action="../auth/logout.php" method="post" accept-charset="utf-8">
           <input type="hidden" name="action" value="logOut" />
           <button type="submit" class="btn btn-block btn-outline-primary">Log out</button>
@@ -75,7 +75,7 @@ $upic1=$row1['u_pic'];
             <i class="nav-icon fas fa-barcode"></i>
              
               <p>
-                PRODUT
+                PRODUTS
               </p>
             </a>
           </li>
