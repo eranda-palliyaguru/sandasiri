2c04a184cd35094d716abea8f80331dc3fc47a7f
erand-palliyaguru <erandasampath2000@gmail.com>
2022-01-01 11:37:17 +0530

    up

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
 
               <div class="info-box-content">
                 <span class="info-box-text">BILL COUNT</span>
-                <span class="info-box-number">Rs.<?php echo $count; ?></span>
+                <span class="info-box-number"><?php echo $count; ?></span>
               </div>
               <!-- /.info-box-content -->
             </div>
